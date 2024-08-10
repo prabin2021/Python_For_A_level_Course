da9ebd0aeaa6295b11e44699f9488c9ffd1a058a
prabin202121/Simple-Portfolio-Webpage. <sigdelprabin321@gmail.com>
2024-08-10 12:07:34 +0545

    First commit

---
+++ Python_Basics_For_Stack_Queue/useofGlobalVariable.py
@@ -0,0 +1,23 @@
+# #Global variable
+
+# x = "Hi"
+# def function1():
+#     x = "Hey"
+#     print(x)
+# function1()
+# print(x)
+
+
+# #use of global keyword
+
+# y = "Hi"
+# def function1():
+#     global y
+#     y = "Hey"
+#     print(y)
+# function1()
+# print(y)
+
+x= dict(name = "Ram")
+print(x)
+print(type(x))
